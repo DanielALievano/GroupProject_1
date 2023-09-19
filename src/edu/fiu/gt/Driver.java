f33d7916eaae6bb11b05a1997cfe29bd43327799
Alvaro Licea <alvarolicea@gmail.com>
2023-09-19 14:54:00 -0400

    modified driver to match test case from Story 01

--- src/edu/fiu/gt/Driver.java
+++ src/edu/fiu/gt/Driver.java
@@ -4,11 +4,11 @@ package edu.fiu.gt;
  * Driver for the Geek Text system
  *
  * @author Alvaro
- * @version 0.2.0
+ * @version 0.2.1
  * @since 0.2.0
  */
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Sample output");
+        System.out.println("Successfully compiled.");
     }
 }
