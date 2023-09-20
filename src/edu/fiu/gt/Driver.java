018083c84aea0e9510e26e3def52b8aa34d6adca
Alvaro Licea <alvarolicea@gmail.com>
2023-09-19 23:27:59 -0400

    removed versioning, left author comments

--- src/edu/fiu/gt/Driver.java
+++ src/edu/fiu/gt/Driver.java
@@ -4,8 +4,6 @@ package edu.fiu.gt;
  * Driver for the Geek Text system
  *
  * @author Alvaro
- * @version 0.2.1
- * @since 0.2.0
  */
 public class Driver {
     public static void main(String[] args) {
