e4a9f31307a73be6dbcfa7c121dd331debd5b65f
Alvaro Licea <alvarolicea@gmail.com>
2023-11-02 17:34:57 -0400

    renamed driver

---
+++ src/edu/fiu/gt/Driver.java
@@ -0,0 +1,9 @@
+package edu.fiu.gt;
+
+import org.springframework.boot.SpringApplication;
+
+public class Driver {
+    public static void main(String[] args) {
+        SpringApplication.run(Driver.class, args);
+    }
+}
