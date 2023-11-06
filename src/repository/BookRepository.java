a8ac253bb30d0baef6260a480198a8f57cc952cf
Alvaro Licea <alvarolicea@gmail.com>
2023-11-06 18:55:27 -0500

    removed unnecessary local files

--- src/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com .rest.API.repository;
-import com.rest.API.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
-
-}
-    
