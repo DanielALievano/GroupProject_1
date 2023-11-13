6ed1eb78a1d05a4446451985a733a87d47afd3d7
slest002 <133921617+slest002@users.noreply.github.com>
2023-11-13 17:30:36 -0500

    moved directory around

--- src/edu/fiu/gt/bookdetails/BookstoreApplicationTests.java
+++ src/edu/fiu/gt/bookdetails/BookstoreApplicationTests.java
@@ -1,6 +1,6 @@
-package edu.fiu.gt;
+package edu.fiu.gt.bookdetails;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- src/main/java/edu/fiu/gt/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.fiu.gt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.fiu.gt.Book;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
