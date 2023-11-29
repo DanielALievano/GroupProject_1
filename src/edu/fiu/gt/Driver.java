ccceba2da527ac03957552488e3143fe8e246f14
Alvaro Licea <alvarolicea@gmail.com>
2023-11-28 20:30:52 -0500

    added driver to common package

---
+++ src/edu/fiu/gt/Driver.java
@@ -0,0 +1,11 @@
+package edu.fiu.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Driver {
+    public static void main(String[] args) {
+        SpringApplication.run(Driver.class, args);
+    }
+}
